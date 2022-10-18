83ebcc0016951089e1c89c8bdaa60e9fb2bcd1fb
ahart1 <arhart555@gmail.com>
2022-10-17 22:47:19 -0400

    Remove files for website written directly in html

--- myJavaScripts.js
+++
@@ -1,9 +0,0 @@
-/* function for hamburger icon */
-function myFunction() {
-  		var x = document.getElementById("myTopnav");
-  		if (x.className === "topnav-right") {
-  			x.className += " responsive";
-  		} else {
-  			x.className = "topnav-right";
-  		}
-  	}
